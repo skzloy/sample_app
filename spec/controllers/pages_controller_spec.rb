5233c3a41ddd4589fe7b71ef4426695e9de12a07
skzloy <dmchetverikov@gmail.com>
2011-08-25 12:24:33 +0400

    Removed default Rails page

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,9 @@ require 'spec_helper'
 
 describe PagesController do
 render_views
+	before(:each) do
+		@base_title = "Ruby on Rails Tutorial Sample App"
+	end
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
@@ -11,7 +14,7 @@ render_views
 	it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
+                        :content => @base_title + " | Home")
     end
   end
 
@@ -25,7 +28,7 @@ render_views
       get 'contact'
       response.should have_selector("title",
                         :content =>
-                          "Ruby on Rails Tutorial Sample App | Contact")
+                          @base_title + " | Contact")
     end
   end
   
@@ -39,7 +42,20 @@ render_views
       get 'about'
       response.should have_selector("title",
                         :content =>
-                          "Ruby on Rails Tutorial Sample App | About")
+                          @base_title + " | About")
+    end
+  end
+  describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+	
+	it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                        :content =>
+                          @base_title + " | Help")
     end
   end
 end
